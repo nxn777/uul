94a927b87e5387afe761ebd8d3db994d1af38ffc
nvnemtin <nxn@inbox.ru>
2021-01-02 13:26:42 -0600

    UUL-1: BottomNavBar design was updated

--- lib/navigation/bottom_navigation.dart
+++ lib/navigation/bottom_navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:UUL_Gym/constants/dimens.dart';
 import 'tabs.dart';
 
 class BottomNavigation extends StatelessWidget {
@@ -9,27 +10,58 @@ class BottomNavigation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      items: [
-        _buildItem(TabItem.NEWS),
-        _buildItem(TabItem.SCHEDULE),
-        _buildItem(TabItem.USER_PROFILES),
-      ],
-      onTap: (index) => onSelectTab(
-        TabItem.values[index],
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(kLargeBorderRadius), topLeft: Radius.circular(kLargeBorderRadius)),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(kLargeBorderRadius),
+          topRight: Radius.circular(kLargeBorderRadius),
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Colors.white,
+          items: [
+            _buildItem(TabItem.NEWS),
+            _buildItem(TabItem.SCHEDULE),
+            _buildItem(TabItem.USER_PROFILES),
+          ],
+          onTap: (index) => onSelectTab(
+            TabItem.values[index],
+          ),
+        ),
       ),
     );
   }
 
   BottomNavigationBarItem _buildItem(TabItem tabItem) {
     return BottomNavigationBarItem(
-      icon: Icon(
-        Icons.layers,
+      icon: FaIcon(
+        _getTabIcon(tabItem),
         color: _colorTabMatching(tabItem),
       ),
       label: tabName[tabItem],
     );
+  }
+
+  IconData _getTabIcon(TabItem item) {
+    IconData icon;
+    switch (item) {
+      case TabItem.NEWS:
+        icon = FontAwesomeIcons.newspaper;
+        break;
+      case TabItem.SCHEDULE:
+        icon = FontAwesomeIcons.list;
+        break;
+      case TabItem.USER_PROFILES:
+        icon = FontAwesomeIcons.user;
+        break;
+    }
+    return icon;
   }
 
   Color _colorTabMatching(TabItem item) {
--- lib/navigation/tabs.dart
+++ lib/navigation/tabs.dart
@@ -6,7 +6,7 @@ enum TabItem { NEWS, SCHEDULE, USER_PROFILES }
 const Map<TabItem, String> tabName = {TabItem.NEWS: "News", TabItem.SCHEDULE: "Schedule", TabItem.USER_PROFILES: "Profiles"};
 
 const Map<TabItem, Color> activeTabColor = {
-  TabItem.NEWS: kAccentColor,
-  TabItem.SCHEDULE: kAccentColor,
-  TabItem.USER_PROFILES: kAccentColor,
+  TabItem.NEWS: Colors.black,
+  TabItem.SCHEDULE: Colors.black,
+  TabItem.USER_PROFILES: Colors.black,
 };
