917a5c502f955becec75d503c0880ca709f288ac
nvnemtin <nxn@inbox.ru>
2021-01-22 18:02:30 -0600

    UUL-1: UULIconButton was implemented

---
+++ lib/widgets/button/u_u_l_icon_button.dart
@@ -0,0 +1,47 @@
+import 'package:UUL_Gym/constants/color_constants.dart';
+import 'package:UUL_Gym/constants/dimens.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class UULIconButton extends StatelessWidget {
+  final void Function() onTap;
+  final IconData icon;
+
+  final Color color;
+  final Color backgroundColor;
+  final double side;
+
+  UULIconButton({this.icon, this.onTap, Color color, Color backgroundColor, double side})
+      : color = color == null ? kAccentColor : color,
+        backgroundColor = backgroundColor == null ? Colors.black12.withOpacity(0.05) : backgroundColor,
+        side = side == null ? kSpacingXXLarge : side;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(kDefaultBorderRadius),
+      child: Material(
+        color: backgroundColor,
+        child: InkWell(
+          onTap: this.onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(kSpacingXXSmall),
+            child: Container(
+              height: side,
+              width: side,
+              //color: Colors.white,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(kDefaultBorderRadius - kSpacingXXSmall), color: Colors.white),
+              child: Center(
+                child: FaIcon(
+                  this.icon,
+                  color: this.color,
+                  size: side - kSpacingMedium,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/user/current_user_card.dart
+++ lib/widgets/user/current_user_card.dart
@@ -2,6 +2,7 @@ import 'package:UUL_Gym/constants/color_constants.dart';
 import 'package:UUL_Gym/constants/dimens.dart';
 import 'package:UUL_Gym/models/user.dart';
 import 'package:UUL_Gym/widgets/avatars/bundled_avatar.dart';
+import 'package:UUL_Gym/widgets/button/u_u_l_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -19,25 +20,20 @@ class CurrentUserCard extends StatelessWidget {
             height: kSpacingXXLarge,
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              FlatButton.icon(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.star,
-                    color: kAccentColor,
-                  ),
-                  label: Text("")),
+              UULIconButton(
+                icon: FontAwesomeIcons.star,
+                onTap: () {},
+              ),
               BundledAvatar(
                 height: kSpacingHuge * 1.5,
                 imageSrc: user?.avatarImageSrc == null ? "assets/defaults/default_user3.png" : user.avatarImageSrc,
               ),
-              FlatButton.icon(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.star,
-                    color: kAccentColor,
-                  ),
-                  label: Text("")),
+              UULIconButton(
+                icon: FontAwesomeIcons.star,
+                onTap: () {},
+              ),
             ],
           ),
         ],
