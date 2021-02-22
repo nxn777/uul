898ac85ba033fdacd7e934214a243294bf56bf72
nvnemtin <nxn@inbox.ru>
2021-02-22 17:27:35 -0600

    UUL-1: Empty data placeholders were added to the schedule screen

--- userstories/timeslots/timeslots_presentation/lib/timeslots/time_slot_screen.dart
+++ userstories/timeslots/timeslots_presentation/lib/timeslots/time_slot_screen.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:arch_components/arch_components.dart';
 import 'package:common/common.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:widgets/widgets.dart';
 
 import 'book/book_time_slot_screen.dart';
@@ -13,71 +13,100 @@ import 'time_slots_viewmodel.dart';
 
 class TimeSlotScreen extends StatelessWidget with ViewStateScreen<TimeSlotsViewModel> {
   @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<TimeSlotsViewModel>(
-      create: (context) => TimeSlotsScreenBuilder.buildAndStartVM(context),
-      child: Consumer<TimeSlotsViewModel>(
-        builder: (context, viewModel, child) {
-          return Scaffold(
-            body: SafeArea(
-              child: buildBody(viewModel, context),
-            ),
-          );
-        },
-      ),
-    );
-  }
+  TimeSlotsViewModel Function(BuildContext p1) vmCreator()  => TimeSlotsScreenBuilder.buildAndStartVM;
 
   Widget buildIdleState(TimeSlotsViewModel viewModel, BuildContext context) {
+    return OrientationBuilder(builder: (context, orientation) {
+      if (orientation == Orientation.portrait) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: _getScreenChildren(context, viewModel, false),
+        );
+      } else {
+        return ListView(
+          children: _getScreenChildren(context, viewModel, true),
+        );
+      }
+    });
+  }
+
+  List<Widget> _getScreenChildren(BuildContext context, TimeSlotsViewModel viewModel, bool insideListView) {
     TimeSlotsScreenObject screenObject = viewModel.viewState.value;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        ScreenTitle("Schedule"),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(kSpacingMedium, 0, kSpacingMedium, kSpacingMedium),
-          child: GymList(
-            gyms: screenObject.gyms,
-            onGymTapped: viewModel.changeActiveGym,
-            gymSelectedChecker: (gym) => gym.id == screenObject.activeGymId,
-          ),
+    return [
+      ScreenTitle("Schedule"),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(kSpacingMedium, 0, kSpacingMedium, kSpacingMedium),
+        child: GymList(
+          gyms: screenObject.gyms,
+          onGymTapped: viewModel.changeActiveGym,
+          gymSelectedChecker: (gym) => gym.id == screenObject.activeGymId,
         ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(kSpacingMedium, kSpacingMedium, kSpacingMedium, kSpacingMedium),
-          child: DayListAnimated(
-            currentWeek: screenObject.currentWeek,
-            activeDate: screenObject.activeDate,
-            currentDate: screenObject.currentDate,
-            onTap: viewModel.changeActiveDate,
-          ),
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(kSpacingMedium, kSpacingMedium, kSpacingMedium, kSpacingMedium),
+        child: DayListAnimated(
+          currentWeek: screenObject.currentWeek,
+          activeDate: screenObject.activeDate,
+          currentDate: screenObject.currentDate,
+          onTap: viewModel.changeActiveDate,
         ),
-        Expanded(
-          child: TimeSlotList(
-            timeSlots: screenObject.timeSlots,
-            isCurrentDateActive: screenObject.isCurrentDateActive,
-            rules: screenObject.rules,
-            onTap: (timeSlot) {
-              showModalBottomSheet(
-                context: context,
-                isScrollControlled: true,
-                builder: (context) => BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: .7, sigmaY: .7),
-                  child: SingleChildScrollView(
-                    child: Container(
-                      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + kBottomNavBarHeight),
-                      child: BookTimeSlotScreen(
-                        timeSlot: timeSlot,
-                        gymTitle: screenObject.activeGym.getTitle(),
-                        placesLeft: screenObject.rules.personsPerTimeSlot - timeSlot.occupiedBy.length,
-                      ),
+      ),
+      _getScheduleContent(context, screenObject, viewModel.viewState.status == ViewStatus.LOADING, insideListView)
+    ];
+  }
+
+  Widget _getScheduleContent(BuildContext context, TimeSlotsScreenObject screenObject, bool isLoading, bool insideListView) {
+    if (screenObject.timeSlots.isNotEmpty || isLoading) {
+      return Expanded(
+        child: TimeSlotList(
+          insideListView: insideListView,
+          timeSlots: screenObject.timeSlots,
+          isCurrentDateActive: screenObject.isCurrentDateActive,
+          rules: screenObject.rules,
+          onTap: (timeSlot) {
+            showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) => BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: .7, sigmaY: .7),
+                child: SingleChildScrollView(
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom + kBottomNavBarHeight),
+                    child: BookTimeSlotScreen(
+                      timeSlot: timeSlot,
+                      gymTitle: screenObject.activeGym.getTitle(),
+                      placesLeft: screenObject.rules.personsPerTimeSlot - timeSlot.occupiedBy.length,
                     ),
                   ),
                 ),
-              );
-            },
-          ),
-        )
-      ],
-    );
+              ),
+            );
+          },
+        ),
+      );
+    }
+    if(screenObject.currentDate.isBefore(screenObject.activeDate)) {
+      return Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FaIcon(FontAwesomeIcons.earlybirds, size: kSpacingXLarge, color: kAccentColor,),
+            SizedBox(height: kSpacingMedium,),
+            Text("Gym booking is not available for this day yet", style: kRegularActiveTextStyle,),
+          ],
+        ),
+      );//
+    } else {
+      return Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FaIcon(FontAwesomeIcons.question, size: kSpacingXLarge, color: kAccentColor,),
+            SizedBox(height: kSpacingMedium,),
+            Text("No data for this day", style: kRegularActiveTextStyle,),
+          ],
+        ),
+      );//
+    }
   }
 }
--- widgets/lib/timeslot/time_slot_list.dart
+++ widgets/lib/timeslot/time_slot_list.dart
@@ -4,12 +4,13 @@ import 'package:flutter/material.dart';
 import 'time_slot_tile.dart';
 
 class TimeSlotList extends StatelessWidget {
+  final bool insideListView;
   final Function(TimeSlot) onTap;
   final List<TimeSlot> timeSlots;
   final bool isCurrentDateActive;
   final Rules rules;
 
-  TimeSlotList({@required this.timeSlots, @required this.isCurrentDateActive, @required this.rules, @required this.onTap});
+  TimeSlotList({@required this.timeSlots, @required this.isCurrentDateActive, @required this.rules, @required this.onTap, @required this.insideListView});
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +22,8 @@ class TimeSlotList extends StatelessWidget {
         onTap: onTap,
       ),
       itemCount: timeSlots.length,
+      shrinkWrap: insideListView ? true : false,
+      physics: insideListView ? ClampingScrollPhysics() : null,
     );
   }
 }
